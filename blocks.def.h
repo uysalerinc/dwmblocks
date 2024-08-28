1b5e7a2fdd073479e5a3c7486d6a7154178d8e60
Erinç Uysal <erincuysal@outlook.com>
2024-08-28 14:25:09 +0300

    no idea lol

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		            /*Update Interval*/  /*Update Signal*/
+	{" ",           "dwm_cpu",	                            4,	    	        0},
 	{" ",           "dwm_memory",	                            30,	    	        0},
 	{"",            "dwm_volume",                  		    0,	    	        10},
 	{"",            "dwm_datetime",      		    5,	    	        0},
